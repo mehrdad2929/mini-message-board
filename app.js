0a3e07a547a4b13cb4a07710043250bcba41ca92
mehrdad <mehrdads416@gmail.com>
2025-09-25 15:28:41 +0330

    fixing style for vercel

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(compression()); // Compresses res body (gzip, etc.)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', messageRouter)
 
 app.use((err, req, res, next) => {
